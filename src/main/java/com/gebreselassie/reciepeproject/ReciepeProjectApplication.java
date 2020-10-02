d5e9626a9a0914428870df3e575c894ef2de80ef
Haftom-Mesfin <sadtime1906@gmail.com>
2020-10-02 15:22:22 -0500

    reciepe project skeleton.

---
+++ src/main/java/com/gebreselassie/reciepeproject/ReciepeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gebreselassie.reciepeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReciepeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReciepeProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gebreselassie/reciepeproject/ReciepeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gebreselassie.reciepeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciepeProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
