28ab2af2d339372bc4e7abece56f793e2743bf05
Haftom-Mesfin <sadtime1906@gmail.com>
2020-10-02 21:18:27 -0500

    Difficulty enumeration added.

---
+++ src/main/java/com/gebreselassie/reciepeproject/domain/Difficulty.java
@@ -0,0 +1,5 @@
+package com.gebreselassie.reciepeproject.domain;
+
+public enum Difficulty {
+    EASY,MODERATE,HARD
+}
--- src/main/java/com/gebreselassie/reciepeproject/domain/Recipe.java
+++ src/main/java/com/gebreselassie/reciepeproject/domain/Recipe.java
@@ -18,7 +18,8 @@ public class Recipe {
     private String url;
     private String directions;
     // to add
-    // private Difficulty difficulty;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
     @Lob // BLOB
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -112,5 +113,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
