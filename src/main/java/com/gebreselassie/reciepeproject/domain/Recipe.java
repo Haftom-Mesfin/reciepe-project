4e12236ddcb2f18f64d1167192dab412faec9471
Haftom-Mesfin <sadtime1906@gmail.com>
2020-10-02 20:38:59 -0500

    Recipe and Ingredient made entities.

--- src/main/java/com/gebreselassie/reciepeproject/domain/Recipe.java
+++ src/main/java/com/gebreselassie/reciepeproject/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.gebreselassie.reciepeproject.domain;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -22,6 +23,8 @@ public class Recipe {
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     public Long getId() {
         return id;
@@ -101,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
