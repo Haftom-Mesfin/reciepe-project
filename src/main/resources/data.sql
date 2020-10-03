2e40b53e24e67334c5c65343654f5341f864d383
Haftom-Mesfin <sadtime1906@gmail.com>
2020-10-02 22:27:27 -0500

    In memeory h2 database tested.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category(description) VALUES ('American');
+INSERT INTO category(description) VALUES ('Tigrian');
+INSERT INTO category(description) VALUES ('Eritrean');
+INSERT INTO category(description) VALUES ('Ethiopian');
+INSERT INTO unit_of_measure(description) VALUES ('Teaspoon');
+INSERT INTO unit_of_measure(description) VALUES ('Tablespoon');
+INSERT INTO unit_of_measure(description) VALUES ('Cup');
+INSERT INTO unit_of_measure(description) VALUES ('Pinch');
+INSERT INTO unit_of_measure(description) VALUES ('Ounce');
